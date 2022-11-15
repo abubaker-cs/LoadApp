791682bbb1ac8266cf9b75582eee58e730a8bb0a
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-11-16 03:37:02 +0500

    Working on DetailActivity.kt

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,11 @@
 package com.udacity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.udacity.Constants.KEY_FILENAME
+import com.udacity.Constants.KEY_STATUS
 import com.udacity.databinding.ActivityDetailBinding
 import com.udacity.databinding.ContentDetailBinding
 
@@ -10,9 +13,6 @@ class DetailActivity : AppCompatActivity() {
 
     private lateinit var _binding: ActivityDetailBinding
     private lateinit var _bindingDetailContent: ContentDetailBinding
-
-    private var name = ""
-    private var status = ""
 
     /**
      * onCreate()
@@ -29,6 +29,20 @@ class DetailActivity : AppCompatActivity() {
         setSupportActionBar(_binding.toolbar)
 
         //
+        _bindingDetailContent.fileName.text = intent.getStringExtra(KEY_FILENAME)
+        _bindingDetailContent.status.text = intent.getStringExtra(KEY_STATUS)
+
+        //
+        if (intent.getStringExtra(KEY_STATUS) == "Failed") {
+            _bindingDetailContent.status.setTextColor(Color.RED)
+        } else {
+            _bindingDetailContent.status.setTextColor(Color.GREEN)
+        }
+
+        //
+        _bindingDetailContent.btnOk.setOnClickListener {
+            finish()
+        }
 
     }
 
