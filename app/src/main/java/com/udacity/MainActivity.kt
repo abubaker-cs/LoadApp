dfd3382ea1d95f81016e1421862ad7c395dd7e5f
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-11-16 22:42:00 +0500

    Working...

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -8,31 +9,50 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.udacity.Constants.KEY_FILENAME
+import com.udacity.Constants.KEY_STATUS
 import com.udacity.databinding.ActivityMainBinding
+import com.udacity.databinding.ContentMainBinding
+import com.udacity.utils.ButtonState
 
 
 class MainActivity : AppCompatActivity() {
 
+    // Binding
     private lateinit var _binding: ActivityMainBinding
+    private lateinit var _bindingDetailContent: ContentMainBinding
 
+    //
     private var downloadID: Long = 0
 
+    //
     private lateinit var notificationManager: NotificationManager
+
+    //
     private lateinit var pendingIntent: PendingIntent
-    private lateinit var action: NotificationCompat.Action
+
+    //
+    private lateinit var fileName: String
+
+    //
+    private lateinit var url: String
 
     /**
-     * onCreate()
+     * override 01 - onCreate()
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Inflate Layout: @layout/activity_main.xml
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _bindingDetailContent = _binding.contentMain
 
         //
         _binding.lifecycleOwner = this
@@ -41,24 +61,104 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        _bindingDetailContent.customButton.setOnClickListener {
+            if (::url.isInitialized) {
 
-//        binding.custom_button.setOnClickListener {
-//            download()
-//        }
+                // Custom Button State: Loading
+                _bindingDetailContent.customButton.buttonState = ButtonState.Loading
+                download()
 
+            } else
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.select_from_list),
+                    Toast.LENGTH_SHORT
+                ).show()
+        }
+
+        // Radio Button : Clicked ?
+        _binding.contentMain.radioGroup.setOnCheckedChangeListener { _, index ->
+            when (index) {
+                R.id.radio_glide -> {
+                    url = URL_GLIDE
+                    fileName = getString(R.string.source_glide)
+                }
+
+                R.id.radio_retrofit -> {
+                    url = URL_RETROFIT
+                    fileName = getString(R.string.source_retrofit)
+                }
+                R.id.radio_load_app -> {
+                    url = URL_UDACITY
+                    fileName = getString(R.string.source_udacity)
+                }
+
+            }
+
+        }
+
+    }
+
+    /**
+     * override 02 - onResume()
+     */
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+    }
+
+    /**
+     * override 03 - onPause()
+     */
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
     }
 
     /**
      * Receiver
      */
     private val receiver = object : BroadcastReceiver() {
+
         override fun onReceive(context: Context?, intent: Intent?) {
+
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            if (id == -1L)
+                return
+
+            id?.let { intentId ->
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                val query = DownloadManager.Query()
+
+                query.setFilterById(intentId)
+
+                val cursor = downloadManager.query(query)
+
+                if (cursor.moveToFirst()) {
+
+                    val index = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+
+                    val downloadStatus =
+                        if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(index))
+                            getString(R.string.success_status)
+                        else
+                            getString(R.string.failed_status)
+
+                    sendNotifications(downloadStatus)
+
+                    // Custom Button State: Completed
+                    _bindingDetailContent.customButton.buttonState = ButtonState.Completed
+
+                }
+
+            }
+
         }
     }
 
     /**
-     * download()
+     * fun 01 - download()
      */
     private fun download() {
         val request =
@@ -72,6 +172,70 @@ class MainActivity : AppCompatActivity() {
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    /**
+     * fun 02 - sendNotifications()
+     */
+    private fun sendNotifications(status: String) {
+
+        createChannel()
+
+        intent = Intent(applicationContext, DetailActivity::class.java)
+        intent.putExtra(KEY_STATUS, status)
+        intent.putExtra(KEY_FILENAME, fileName)
+
+        pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(applicationContext.getString(R.string.notification_title))
+            .setContentText(getString(R.string.loading_completed))
+            .setAutoCancel(true)
+            .addAction(
+                R.drawable.abc_vector_test,
+                getString(R.string.see_result),
+                pendingIntent
+            )
+
+        notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    /**
+     * fun 03 - createChannel()
+     */
+    private fun createChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channel.enableVibration(true)
+            channel.enableLights(true)
+
+            channel.description = getString(R.string.loading_completed)
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(channel)
+
+        }
     }
 
     /**
@@ -92,9 +256,9 @@ class MainActivity : AppCompatActivity() {
         /**
          * Channel
          */
-        private const val NOTIFICATION_ID = ""
+        private const val NOTIFICATION_ID = 1
         private const val CHANNEL_ID = "channelId"
-        private const val CHANNEL_NAME = ""
+        private const val CHANNEL_NAME = "notification_channel"
 
     }
 
